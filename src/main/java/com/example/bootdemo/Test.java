b5bcb7e248fb63978fc19f6f0b0cbdcbb6d6a388
unknown <panpan@panpan.linekongdomain.com>
2018-05-17 16:46:40 +0800

    测试添加一行代码

--- src/main/java/com/example/bootdemo/Test.java
+++ src/main/java/com/example/bootdemo/Test.java
@@ -2,6 +2,7 @@ package com.example.bootdemo;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("========= start  ==========");
         for(int i = 0 ; i<10 ;i++) {
             System.out.println("hello+"+i);
         }
